8278dae32cba436b0636b4726ea10da26874995c
sobuj647 <sobujdevsharma43@gmail.com>
2025-08-28 13:07:54 +0600

    add heart icon function js

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-console.log('Love')
+// Heart Icon Function:
+document.getElementById("heart-icon-btn").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn2").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn3").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn4").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn5").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn6").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn7").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn8").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
+document.getElementById("heart-icon-btn9").addEventListener('click',function(e){ 
+    e.preventDefault()
+    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    
+    const newAvailableHeart=  1+ availableHeart;
+    document.getElementById("available-heart").innerText=newAvailableHeart
+    
+})
